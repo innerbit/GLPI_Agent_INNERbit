1cf784d2a0e8b7c961cd9f61397beeb27bad2d08
INNERbit Informática <suporte@innerbit.com.br>
2022-07-14 15:41:22 -0300

    Update Menu_Install.bat

--- Menu_Install.bat
+++ Menu_Install.bat
@@ -1,8 +1,8 @@
 @ECHO off
 REM - Executar como Administrador
-REM     if "%~s0"=="%~s1" ( cd %~sp1 & shift ) else (
-REM        echo CreateObject^("Shell.Application"^).ShellExecute "%~s0","%~0 %*","","runas",1 >"%tmp%%~n0.vbs" & "%tmp%%~n0.vbs" & del /q "%tmp%%~n0.vbs" & goto :eof
-REM )
+     if "%~s0"=="%~s1" ( cd %~sp1 & shift ) else (
+        echo CreateObject^("Shell.Application"^).ShellExecute "%~s0","%~0 %*","","runas",1 >"%tmp%%~n0.vbs" & "%tmp%%~n0.vbs" & del /q "%tmp%%~n0.vbs" & goto :eof
+ )
 REM Cliente
 				echo # - - - - - - - - - - - - - - - - - - - - - - - - - #
 				echo #            NOME DA EMPRESA/CLIENTE?               #
